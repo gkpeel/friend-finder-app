57a29ababe9046635f6aaec29040ea9d97447784
gkpeel <gkpeel@gmail.com>
2018-11-01 12:55:31 -0400

    created survey and '/' and '/survey' get routes

--- server.js
+++ server.js
@@ -2,9 +2,19 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
+var PORT = process.env.PORT || 8888;
 
-var PORT = 8888;
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, 'app/public/home.html'));
+});
+
+app.get('/survey', function(req,res){
+    res.sendFile(path.join(__dirname, 'app/public/survey.html'));
+});
 
 app.listen(PORT, function() {
     console.log('Server started in http://localhost:' + PORT);
-})
+});
