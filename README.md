3dac0c21198706b3f61ba0fe702873e958dbeb3d
gkpeel <gkpeel@gmail.com>
2018-11-01 21:53:41 -0400

    Created Readme and improved styling

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# friend-finder-app
-Find people with similar preferences as yours. Boom - instant besties.
+# Culinary Friend-finder App
+Find people with similar culinary preferences as yours. Boom - instant besties.
+
+To use Gut Buddies go to INSERT HEROKU URL to see the intro page:
+
+![Gut Buddies Intro Page](img/homepage.png)
+
+Once there, navigate to the survey:
+![Gut Buddies Survey Page - Top](img/survey-top.png)
+
+Fill out the survey with your preferences:
+![Gut Buddies Survey Page - Middle](img/survey-middle.png)
+
+Once finished, you'll be presented with an image of your new best friend. 
+__Maybe you two should do lunch.__
+![Gut Buddies Survey Page - Bottom](img/survey-result.png)
+
+
+
 
--- app/data/friends.js
+++ app/data/friends.js
@@ -2,12 +2,27 @@ var friendsTable = [
     {
         "name":"Ahmed",
         "photo":"https://thumbs.dreamstime.com/z/smiling-old-man-having-coffee-portrait-looking-happy-33471677.jpg",
-        "scores": ["5", "5", "5", "4", "5", "5", "5", "5", "5", "5"]
+        "scores": ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
     },
     {
         "name":"Geoff",
         "photo": 'https://pbs.twimg.com/profile_images/1038237926331576321/wx4uBti6_400x400.jpg',
         "scores": ["1", "1", "1", "2", "2", "1", "2", "2", "2", "1"]
+    },
+    {
+        "name": "Fred",
+        "photo": 'https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg?auto=compress&cs=tinysrgb&h=350',
+        "scores": ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
+    },
+    {
+        "name": "Alicia", 
+        "photo": 'https://image.shutterstock.com/image-photo/portrait-young-beautiful-cute-cheerful-260nw-666258808.jpg',
+        "scores": ["1", "2", "3", "4", "5", "4", "3", "2", "1"]
+    },
+    {
+        "name": "JAWN",
+        "photo": "https://external-preview.redd.it/eaWHFt9aFd1Wow_p34lk0BRdWX44vJF2BZs54TUXAQw.jpg?auto=webp&s=9afb2447ebecd53ea0a5d601d305094e90a55d6a",
+        "scores": ["1", "2", "3", "1", "1", "1", "3", "5", "3", "1"]
     }
 ];
 
